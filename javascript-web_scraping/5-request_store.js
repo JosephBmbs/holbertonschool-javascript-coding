42913d2411645cfa56c2af86e369f1bcf5203ccd
Youssef_Saad <6270@holbertonstudents.com>
2023-10-11 18:25:19 +0100

    Loripsum,chor: refactor scripte

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,20 +1,27 @@
-#!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
+const fs = require('fs');
 
-request(url, (error, respose, body) => {
+if (process.argv.length !== 4) {
+  console.log('Usage: node 5-request_store.js <URL> <file_path>');
+  process.exit(1);
+}
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
   if (error) {
-    console.log(error);
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('HTTP Error:', response.statusCode);
   } else {
-    const films = JSON.parse(body).results;
-    let ct = 0;
-    for (const film of films) {
-      for (const character of film.characters) {
-        if (character.includes('18')) {
-          ct++;
-        }
+    // Save the response body to the specified file
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) {
+        console.error('Error writing to file:', err);
+      } else {
+        console.log(`Downloaded and saved ${body.length} bytes to ${filePath}`);
       }
-    }
-    console.log(ct);
+    });
   }
 });
