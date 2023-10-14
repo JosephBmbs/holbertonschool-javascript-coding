855da0400eec2826ae09593de3ce2acb18176138
Youssef_Saad <6270@holbertonstudents.com>
2023-10-14 19:42:17 +0100

    utils sets

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,19 +1,41 @@
-const fs = require("fs");
+import fs from 'fs';
 
-exports.readDatabase = (path) =>
-  new Promise((resolve, reject) => {
-    fs.readFile(path, { encoding: "utf-8" }, (err, data) => {
-      try {
-        if (err) reject(Error("Cannot load the database"));
-        const studentsInfo = data
-          .split("\n")
-          .filter((line) => line)
-          .slice(1);
-        return resolve({
-          studentsInfo,
-        });
-      } catch (err) {
-        return reject(Error("Cannot load the database"));
+const readDatabase = (dataPath) => new Promise((resolve, reject) => {
+  if (!dataPath) {
+    reject(new Error('Cannot load the database'));
+  }
+  if (dataPath) {
+    fs.readFile(dataPath, (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      }
+      if (data) {
+        const fileLines = data
+          .toString('utf-8')
+          .trim()
+          .split('\n');
+        const studentGroups = {};
+        const dbFieldNames = fileLines[0].split(',');
+        const studentPropNames = dbFieldNames
+          .slice(0, dbFieldNames.length - 1);
+
+        for (const line of fileLines.slice(1)) {
+          const studentRecord = line.split(',');
+          const studentPropValues = studentRecord
+            .slice(0, studentRecord.length - 1);
+          const field = studentRecord[studentRecord.length - 1];
+          if (!Object.keys(studentGroups).includes(field)) {
+            studentGroups[field] = [];
+          }
+          const studentEntries = studentPropNames
+            .map((propName, idx) => [propName, studentPropValues[idx]]);
+          studentGroups[field].push(Object.fromEntries(studentEntries));
+        }
+        resolve(studentGroups);
       }
     });
-  });
+  }
+});
+
+export default readDatabase;
+module.exports = readDatabase;
